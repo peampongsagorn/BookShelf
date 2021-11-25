4623db6b81a93449ac900d63dc209d810170bbdf
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2021-11-25 17:10:59 +0700

    day1 front1,2

---
+++ frontend/bookshelf/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {BooksComponent } from './books/books.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/bookshelf/src/app/mock-books.ts
@@ -0,0 +1,14 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [
+  { id: 11, name: 'Dr Nice', price: 250, author: 'Songhai', page: 410 },
+  { id: 12, name: 'Narco', price: 200, author: 'Songhai', page: 107  },
+  { id: 13, name: 'Bombasto', price: 50, author: 'Songhai', page: 109  },
+  { id: 14, name: 'Celeritas', price: 350, author: 'Songhai', page: 180  },
+  { id: 15, name: 'Magneta', price: 210, author: 'Songhai', page: 108  },
+  { id: 16, name: 'RubberMan', price: 750, author: 'Songhai', page: 410  },
+  { id: 17, name: 'Dynama', price: 290, author: 'Songhai', page: 140  },
+  { id: 18, name: 'Dr IQ', price: 300, author: 'Songhai', page: 150  },
+  { id: 19, name: 'Magma', price: 410, author: 'Songhai', page: 110  },
+  { id: 20, name: 'Tornado', price: 550, author: 'Songhai', page: 510  }
+];
